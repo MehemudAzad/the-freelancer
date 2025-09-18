2792e9beac02317051f4ec3aa92464dc0522f602
Khalid Hasan Tuhin <khalidhasantuhin@Khalids-MacBook-Air.local>
2025-09-18 15:01:15 +0600

    mypart

---
+++ shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+rm -rf auth-service/postgres-data 
+rm -rf gig-service/postgres-data
+rm -rf workspace-service/postgres-data 
+rm -rf notification-service/postgres-data
+rm -rf job-proposal-servic/postgres-data 
+rm -rf payment-service/postgres-data 
+rm -rf ai-service/postgres-data 
